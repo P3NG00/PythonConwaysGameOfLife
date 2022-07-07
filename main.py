692f936ca2a0e019c72b51f1bad5cb99e9c09703
P3NG00 <p3ng00minecraft@gmail.com>
2022-07-07 10:47:57 -0600

    created togglable 'units'

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
 import pygame
 from pygame import Color
+from pygame import Surface
 from pygame import Vector2
 
 
 # constants
-SURFACE_SIZE = Vector2(800, 600)
+WINDOW_TITLE = "C.G.O.L"
 CLOCK = pygame.time.Clock()
 FPS = 65.0
-COLOR_BG = Color(0, 0, 0)
-COLOR_CLICK = Color(255, 0, 0)
+COLOR_BG = Color(128, 128, 128)
+COLOR_UNIT_INACTIVE = Color(0, 0, 0)
+COLOR_UNIT_ACTIVE = Color(255, 255, 255)
+UNIT_ARRAY_SQUARE_SIZE = 10
+UNIT_SIZE = 25
+UNIT_SIZE_VECTOR = Vector2(UNIT_SIZE)
+
+
+class Unit:
+
+    def __init__(self, position: Vector2):
+        self.active = False
+        self.position = position
+
+    def draw(self, surface: Surface) -> None:
+        pygame.draw.rect(surface,
+                         COLOR_UNIT_ACTIVE if self.active else COLOR_UNIT_INACTIVE,
+                         (self.position, UNIT_SIZE_VECTOR))
+
 
 # variables
 running = True
-clicked = False
+# TODO figure out why there is no space between each unit
+unit_array = [Unit(Vector2(x * UNIT_SIZE,
+                           y * UNIT_SIZE))
+                            for y in range(UNIT_ARRAY_SQUARE_SIZE)
+                            for x in range(UNIT_ARRAY_SQUARE_SIZE)]
+dirty_array = unit_array.copy()
+surface_size = (800, 600)
 
 # initialize pygame
 pygame.init()
 
 # create surface
-surface = pygame.display.set_mode(SURFACE_SIZE)
+surface = pygame.display.set_mode(surface_size, pygame.RESIZABLE)
+surface.fill(COLOR_BG)
+pygame.display.set_caption(WINDOW_TITLE)
 
 
 # loop
@@ -54,23 +80,25 @@ while running:
 
                     case pygame.BUTTON_LEFT:
 
-                        # left mouse held
-                        clicked = True
+                        # toggle state of selected unit
+                        mouse_pos = pygame.mouse.get_pos()
+                        mouse_pos = (int(mouse_pos[0] / UNIT_SIZE),
+                                     int(mouse_pos[1] / UNIT_SIZE))
+                        mouse_unit = unit_array[(mouse_pos[1] * UNIT_ARRAY_SQUARE_SIZE) + mouse_pos[0]]
+                        mouse_unit.active = not mouse_unit.active
+                        dirty_array.append(mouse_unit)
 
-            case pygame.MOUSEBUTTONUP:
+            case pygame.VIDEORESIZE:
 
-                match event.button:
-
-                    case pygame.BUTTON_LEFT:
-
-                        # left mouse released
-                        clicked = False
+                # update surface size
+                surface_size = (event.w, event.h)
+                # redraw every unit
+                dirty_array = unit_array.copy()
 
     # draw
-    surface.fill(COLOR_BG)
-
-    if clicked:
-        pygame.draw.circle(surface, COLOR_CLICK, pygame.mouse.get_pos(), 5)
+    for unit in dirty_array:
+        unit.draw(surface)
+    dirty_array.clear()
 
     # update display
     pygame.display.flip()
