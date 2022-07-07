10298ad105653e659397f107295af279cdd56c4c
P3NG00 <p3ng00minecraft@gmail.com>
2022-07-07 15:04:43 -0600

    added togglable draw mode using F1; reorganized constants

--- main.py
+++ main.py
@@ -4,19 +4,24 @@ from pygame import Surface
 from pygame import Vector2
 
 
-# constants
+# changable constants
 WINDOW_TITLE = "C.G.O.L"
-CLOCK = pygame.time.Clock()
 FPS = 65.0
 COLOR_BG = Color(64, 64, 64)
 COLOR_UNIT_INACTIVE = Color(0, 0, 0)
 COLOR_UNIT_ACTIVE = Color(255, 255, 255)
 UNIT_ARRAY_SQUARE_SIZE = 20
 UNIT_SIZE = 24
-UNIT_SIZE_VECTOR = Vector2(UNIT_SIZE)
 UNIT_BORDER_SPACE = 1
+
+# dependent constants
+CLOCK = pygame.time.Clock()
+UNIT_SIZE_HALF = UNIT_SIZE / 2
+UNIT_SIZE_VECTOR = Vector2(UNIT_SIZE)
+UNIT_SIZE_VECTOR_HALF = UNIT_SIZE_VECTOR / 2
 TOTAL_UNIT_SIZE = UNIT_SIZE + UNIT_BORDER_SPACE
 TOTAL_UNIT_SIZE_VECTOR = Vector2(TOTAL_UNIT_SIZE)
+TOTAL_UNIT_SIZE_VECTOR_HALF = TOTAL_UNIT_SIZE_VECTOR / 2
 
 
 class Unit:
@@ -26,9 +31,16 @@ class Unit:
         self.position = position
 
     def draw(self, surface: Surface) -> None:
-        pygame.draw.rect(surface,
-                         COLOR_UNIT_ACTIVE if self.active else COLOR_UNIT_INACTIVE,
-                         (self.position, TOTAL_UNIT_SIZE_VECTOR if simulating else UNIT_SIZE_VECTOR))
+        if draw_mode:
+            pygame.draw.rect(surface, self._get_color(), (self.position,
+                             TOTAL_UNIT_SIZE_VECTOR if simulating else UNIT_SIZE_VECTOR))
+        else:
+            pygame.draw.circle(surface, self._get_color(), self.position +
+                              (TOTAL_UNIT_SIZE_VECTOR_HALF if simulating else
+                               UNIT_SIZE_VECTOR_HALF), UNIT_SIZE_HALF)
+
+    def _get_color(self) -> Color:
+        return COLOR_UNIT_ACTIVE if self.active else COLOR_UNIT_INACTIVE
 
 
 def redraw_all():
@@ -46,15 +58,14 @@ unit_array = [Unit(Vector2((x * TOTAL_UNIT_SIZE) + UNIT_BORDER_SPACE,
                            (y * TOTAL_UNIT_SIZE) + UNIT_BORDER_SPACE))
                             for y in range(UNIT_ARRAY_SQUARE_SIZE)
                             for x in range(UNIT_ARRAY_SQUARE_SIZE)]
-# dirty array starts with copy of reference to the created units
-dirty_array = unit_array.copy()
 surface_size = (TOTAL_UNIT_SIZE_VECTOR * UNIT_ARRAY_SQUARE_SIZE) + Vector2(UNIT_BORDER_SPACE)
+draw_mode = True
 
 # create surface
 pygame.init()
-surface = pygame.display.set_mode(surface_size, pygame.RESIZABLE)
-surface.fill(COLOR_BG)
 pygame.display.set_caption(WINDOW_TITLE)
+surface = pygame.display.set_mode(surface_size, pygame.RESIZABLE)
+redraw_all()
 
 
 # loop
@@ -90,6 +101,12 @@ while running:
                         simulating = not simulating
                         redraw_all()
 
+                    case pygame.K_F1:
+
+                        # toggle draw mode
+                        draw_mode = not draw_mode
+                        redraw_all()
+
             case pygame.MOUSEBUTTONDOWN:
 
                 match event.button:
@@ -101,8 +118,10 @@ while running:
                             mouse_pos = pygame.mouse.get_pos()
                             mouse_pos = (int(mouse_pos[0] / TOTAL_UNIT_SIZE),
                                          int(mouse_pos[1] / TOTAL_UNIT_SIZE))
+                            # check click was in bounds
                             if mouse_pos[0] < UNIT_ARRAY_SQUARE_SIZE and \
                                mouse_pos[1] < UNIT_ARRAY_SQUARE_SIZE:
+                                # add selected unit to dirty list
                                 mouse_unit = unit_array[(mouse_pos[1] * UNIT_ARRAY_SQUARE_SIZE) + mouse_pos[0]]
                                 mouse_unit.active = not mouse_unit.active
                                 dirty_array.append(mouse_unit)
@@ -114,7 +133,7 @@ while running:
                 redraw_all()
 
     # draw
-    if len(dirty_array) is not 0:
+    if len(dirty_array) != 0:
         for unit in dirty_array:
             unit.draw(surface)
         pygame.display.flip()
