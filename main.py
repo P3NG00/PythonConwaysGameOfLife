f551d8d1632c7fab5ecaec4ba0d3469389833006
P3NG00 <p3ng00minecraft@gmail.com>
2022-07-08 09:57:47 -0600

    added working simulation; other code changes

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import pygame
 from pygame import Color
 from pygame import Surface
@@ -10,15 +11,24 @@ FPS = 65.0
 COLOR_BG = Color(64, 64, 64)
 COLOR_UNIT_INACTIVE = Color(0, 0, 0)
 COLOR_UNIT_ACTIVE = Color(255, 255, 255)
-UNIT_ARRAY_SQUARE_SIZE = 20
-UNIT_SIZE = 24
+SIMULATION_TIMER_SECONDS = 0.25
+UNIT_ARRAY_SQUARE_SIZE = 50
+UNIT_SIZE = 14
 UNIT_BORDER_SPACE = 1
+
+
+def seconds_to_frames(seconds: float) -> int:
+    """returns the amount of frames equivalent to the given seconds"""
+    return math.ceil(seconds * FPS)
+
 
 # dependent constants
 CLOCK = pygame.time.Clock()
-UNIT_SIZE_HALF = UNIT_SIZE / 2
+UNIT_SIZE_HALF = math.ceil(UNIT_SIZE / 2)
 UNIT_SIZE_VECTOR = Vector2(UNIT_SIZE)
 UNIT_SIZE_VECTOR_HALF = UNIT_SIZE_VECTOR / 2
+UNIT_ARRAY_SQUARE_SIZE_RANGE = range(UNIT_ARRAY_SQUARE_SIZE)
+SIMULATION_TIMER_FRAMES = seconds_to_frames(SIMULATION_TIMER_SECONDS)
 TOTAL_UNIT_SIZE = UNIT_SIZE + UNIT_BORDER_SPACE
 TOTAL_UNIT_SIZE_VECTOR = Vector2(TOTAL_UNIT_SIZE)
 TOTAL_UNIT_SIZE_VECTOR_HALF = TOTAL_UNIT_SIZE_VECTOR / 2
@@ -28,9 +38,11 @@ class Unit:
 
     def __init__(self, position: Vector2):
         self.active = False
+        self.active_last = self.active
         self.position = position
 
     def draw(self, surface: Surface) -> None:
+        """draws the unit to the given surface at its position"""
         if draw_mode:
             pygame.draw.rect(surface, self._get_color(), (self.position,
                              TOTAL_UNIT_SIZE_VECTOR if simulating else UNIT_SIZE_VECTOR))
@@ -40,24 +52,31 @@ class Unit:
                                UNIT_SIZE_VECTOR_HALF), UNIT_SIZE_HALF)
 
     def _get_color(self) -> Color:
+        """returns the appropriate color for this unit's active state"""
         return COLOR_UNIT_ACTIVE if self.active else COLOR_UNIT_INACTIVE
 
 
-def redraw_all():
+def redraw_all() -> None:
+    """clears screen and adds all units to dirty_array to be redrawn"""
     global dirty_array
     # refill surface
     surface.fill(COLOR_BG)
     # mark all units dirty
-    dirty_array = unit_array.copy()
+    dirty_array.clear()
+    for _unit in unit_array:
+        for unit in _unit:
+            dirty_array.append(unit)
 
 
 # variables
 running = True
 simulating = False
-unit_array = [Unit(Vector2((x * TOTAL_UNIT_SIZE) + UNIT_BORDER_SPACE,
-                           (y * TOTAL_UNIT_SIZE) + UNIT_BORDER_SPACE))
-                            for y in range(UNIT_ARRAY_SQUARE_SIZE)
-                            for x in range(UNIT_ARRAY_SQUARE_SIZE)]
+simulation_timer = SIMULATION_TIMER_FRAMES
+unit_array = [[Unit(Vector2((x * TOTAL_UNIT_SIZE) + UNIT_BORDER_SPACE,
+                            (y * TOTAL_UNIT_SIZE) + UNIT_BORDER_SPACE))
+                             for x in UNIT_ARRAY_SQUARE_SIZE_RANGE]
+                             for y in UNIT_ARRAY_SQUARE_SIZE_RANGE]
+dirty_array = []
 surface_size = (TOTAL_UNIT_SIZE_VECTOR * UNIT_ARRAY_SQUARE_SIZE) + Vector2(UNIT_BORDER_SPACE)
 draw_mode = True
 
@@ -99,6 +118,8 @@ while running:
 
                         # toggle simulation
                         simulating = not simulating
+                        if not simulating:
+                            simulation_timer = SIMULATION_TIMER_FRAMES
                         redraw_all()
 
                     case pygame.K_F1:
@@ -121,17 +142,67 @@ while running:
                             # check click was in bounds
                             if mouse_pos[0] < UNIT_ARRAY_SQUARE_SIZE and \
                                mouse_pos[1] < UNIT_ARRAY_SQUARE_SIZE:
-                                # add selected unit to dirty list
-                                mouse_unit = unit_array[(mouse_pos[1] * UNIT_ARRAY_SQUARE_SIZE) + mouse_pos[0]]
+                                # find selected unity, modify active state, and add to dirty list
+                                mouse_unit = unit_array[mouse_pos[1]][mouse_pos[0]]
                                 mouse_unit.active = not mouse_unit.active
                                 dirty_array.append(mouse_unit)
+
+    # check if handling simulation
+    if simulating:
+
+        # decrease simulation_timer
+        simulation_timer -= 1
+
+        # check if next simulation update
+        if simulation_timer == 0:
+
+            # reset timer
+            simulation_timer = SIMULATION_TIMER_FRAMES
+
+            # update each unit
+            for y in UNIT_ARRAY_SQUARE_SIZE_RANGE:
+                for x in UNIT_ARRAY_SQUARE_SIZE_RANGE:
+                    # check for neighbors
+                    unit_neighbors = 0
+                    for offset_y in range(3):
+                        for offset_x in range(3):
+                            check_x = x + (offset_x - 1)
+                            check_y = y + (offset_y - 1)
+                            # skip check if centered or out of bounds
+                            if (offset_x == 1 and offset_y == 1) or \
+                               check_x < 0 or \
+                               check_y < 0 or \
+                               check_x > UNIT_ARRAY_SQUARE_SIZE - 1 or \
+                               check_y > UNIT_ARRAY_SQUARE_SIZE - 1:
+                                continue
+                            # if unit was active, increment neighbors
+                            if unit_array[check_y][check_x].active_last:
+                                unit_neighbors += 1
+                    unit = unit_array[y][x]
+                    # if unit is active and has too few or too many neighbors
+                    if unit.active:
+                        if unit_neighbors < 2 or unit_neighbors > 3:
+                            # make unit inactive
+                            unit.active = False
+                            dirty_array.append(unit)
+                    # if unit is not active and has exactly three neighbors
+                    else:
+                        if unit_neighbors == 3:
+                            # make unit active
+                            unit.active = True
+                            dirty_array.append(unit)
 
     # draw
     if len(dirty_array) != 0:
         for unit in dirty_array:
+            # redraw dirty units
             unit.draw(surface)
-        pygame.display.flip()
+            # update dirty units last active state
+            unit.active_last = unit.active
+        # clear dirty unit array
         dirty_array.clear()
+        # update display
+        pygame.display.flip()
 
     # tick fps clock
     CLOCK.tick(FPS)
